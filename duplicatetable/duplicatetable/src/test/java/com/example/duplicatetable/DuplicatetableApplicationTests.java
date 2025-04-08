a36d7e26cb5895949b88cb28ced5f83e03d0d150
unknown <learnersolution2024@gmail.com>
2025-04-08 10:38:40 +0530

    initializr

---
+++ duplicatetable/duplicatetable/src/test/java/com/example/duplicatetable/DuplicatetableApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.duplicatetable;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DuplicatetableApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
