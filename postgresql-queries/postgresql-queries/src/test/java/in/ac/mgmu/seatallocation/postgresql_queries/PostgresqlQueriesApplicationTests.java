721cf1e898aa46e00801d47916f6586334add592
unknown <learnersolution2024@gmail.com>
2025-04-08 15:31:58 +0530

    new initializr file

---
+++ postgresql-queries/postgresql-queries/src/test/java/in/ac/mgmu/seatallocation/postgresql_queries/PostgresqlQueriesApplicationTests.java
@@ -0,0 +1,13 @@
+package in.ac.mgmu.seatallocation.postgresql_queries;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PostgresqlQueriesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
